e4e0df3c996950f45ac83894d119ac366888057c
ComradeRyn <ryanyuncza@gmail.com>
2026-01-12 12:44:34 -0500

    Added a required keyword to transfer request Amount

--- RESTfulBankAPI/Models/Records/TransferRequest.cs
+++ RESTfulBankAPI/Models/Records/TransferRequest.cs
@@ -2,7 +2,7 @@
 
 public record TransferRequest
 {
-    public decimal Amount { get; init; }
+    public required decimal Amount { get; init; }
     public required string SenderId { get; init; }
     public required string ReceiverId { get; init; }
 }
